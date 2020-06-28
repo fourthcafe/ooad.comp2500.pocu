76319d3166274b2292b387a8bb0833dec5cc17f0
fourthcafe <fourthcafe@gmail.com>
2020-06-28 17:24:18 +0900

    md 파일에 '개체 모델링' 주제를 추가한다
    - '모델링 3: 분무기 용량 추가' 내용을 추가한다.

---
+++ src/dev/fourthcafe/objectmodeling/WaterSpray.java
@@ -0,0 +1,36 @@
+package dev.fourthcafe.objectmodeling;
+
+public class WaterSpray {
+	private final int capacity;
+	private int remainingWaterInMl = 0;
+
+	public WaterSpray(int capacity) {
+		this.capacity = capacity;
+	}
+
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+	public int getRemainingWaterInMl() {
+		return remainingWaterInMl;
+	}
+
+
+	void addWater(int amountInMl) {
+		this.remainingWaterInMl += amountInMl;
+		this.remainingWaterInMl = Math.min(this.remainingWaterInMl, capacity);
+	}
+
+
+	void spray() {
+		this.remainingWaterInMl -= Math.min(this.remainingWaterInMl, 5);
+	}
+
+
+	void fillUp() {
+		this.remainingWaterInMl = capacity;
+	}
+}
