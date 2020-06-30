97fc5430add9984433501bbe500882079c75a6da
fourthcafe <fourthcafe@gmail.com>
2020-07-01 00:56:35 +0900

    '화분에 물 주기' 코드를 보강한다

--- src/main/java/dev/fourthcafe/objectmodeling/FlowerPot.java
+++ src/main/java/dev/fourthcafe/objectmodeling/FlowerPot.java
@@ -3,6 +3,7 @@ package dev.fourthcafe.objectmodeling;
 public class FlowerPot {
 	private boolean alive = true;
 	private final int minDailyWaterInMl;
+	private int dailyWaterReceived = 0;
 
 	public FlowerPot(int minDailyWaterInMl) {
 		this.minDailyWaterInMl = minDailyWaterInMl;
@@ -18,8 +19,15 @@ public class FlowerPot {
 
 
 	public void addWater(int amountInMl) {
-		if (amountInMl < minDailyWaterInMl) {
-			alive = false;
+		this.dailyWaterReceived += amountInMl;
+	}
+
+
+	public void liveAnoterDay() {
+		if (this.dailyWaterReceived < this.minDailyWaterInMl) {
+			this.alive = false;
 		}
+
+		this.dailyWaterReceived = 0;
 	}
 }
--- src/test/java/dev/fourthcafe/objectmodeling/ApplicationTest.java
+++ src/test/java/dev/fourthcafe/objectmodeling/ApplicationTest.java
@@ -40,4 +40,24 @@ public class ApplicationTest {
 
 		assertThat(pot.isAlive()).isTrue(); // 실패
 	}
+
+
+	@Test
+	public void 물을_뿌릴_때마다_받은_물_양을_누적() {
+		final WaterSpray waterSpray = new WaterSpray(100);
+		waterSpray.fillUp();
+
+		final FlowerPot pot = new FlowerPot(10);
+
+		for (int i = 0; i < 2; ++i) {
+			int water = waterSpray.getRemainingWaterInMl();
+			waterSpray.spray();
+			water -= waterSpray.getRemainingWaterInMl();
+
+			pot.addWater(water);
+		}
+
+		pot.liveAnoterDay();
+		assertThat(pot.isAlive()).isTrue();
+	}
 }
